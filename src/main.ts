eda902110562e02a0d719e3b9a53541ec29ea8f5
Alexandre Ferreira <alexandreferreira.s@hotmail.com>
2025-12-15 19:47:43 -0300

    feat: create project

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+  import { NestFactory } from '@nestjs/core';
+  import { AppModule } from './app.module';
+  import { ValidationPipe } from '@nestjs/common';
+
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix(process.env.API_PREFIX || 'api');
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
+    const port = process.env.APP_PORT || 3000;
+    await app.listen(port);
+
+    console.log(`API running on http://localhost:${port}/${process.env.API_PREFIX || 'api'}`);
+  }
+
+  bootstrap();
